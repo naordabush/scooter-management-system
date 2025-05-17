23fafe890e843f37b4d2818c78e572100310bac2
naordabush <naordabush@gmail.com>
2025-05-17 21:26:28 +0300

    update cors url

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ import mongoose from "mongoose";
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000", "https://scooter-management-system.vercel.app/login"];
+const allowedOrigins = ["http://localhost:3000", "https://scooter-management-system.vercel.app"];
 
 app.use(
   cors({
@@ -21,6 +21,7 @@ app.use(
     credentials: true,
   })
 );
+
 
 app.use(express.json());
 
