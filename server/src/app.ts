26e88c54680e01dda13e2e69fa4c96b658c2fc8b
naordabush <naordabush@gmail.com>
2025-05-17 20:37:43 +0300

    Add CORS config

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,6 +12,14 @@ import failureRoutes from "./routes/failure.routes";
 
 const app = express();
 
+// Allow requests from your frontend
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use(cors());
 app.use(express.json());
 
